353e6d95077b5565f90fef732a195a8c519110ed
Arturo Reuschenbach Puncernau <a.reuschenbach.puncernau@sap.com>
2021-03-10 15:52:00 +0100

    refactoring

--- app/middleware/debug_headers_middleware.rb
+++ app/middleware/debug_headers_middleware.rb
@@ -6,8 +6,8 @@ class DebugHeadersMiddleware
   def call(env)
     if env['PATH_INFO'] == '/system/headers'
       _, headers, = @app.call(env)
-      body = ""
-      body = headers.map { |k, v| "#{k}=#{v}" }.sort.join("\n") unless headers.blank?
+      headers = headers || {}
+      body = headers.map { |k, v| "#{k}=#{v}" }.sort.join("\n")
       [200, { 'Content-Type' => 'text/plain', 'Content-Length' => body.size.to_s }, [body]]
     else
       @app.call(env)
